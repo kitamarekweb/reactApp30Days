6ae118dde8bd56f26db654d9abe472ca97476849
kitamarekweb <kitamarekweb@gmail.com>
2017-05-11 13:21:03 +0200

    problem commiting throu terminal had to use storm

---
+++ components/app.js
@@ -0,0 +1,11 @@
+class App extends React.Component {
+    render() {
+        return (
+            <div className="notificationsFrame">
+                <div className="panel">
+                    {/*sdfasdf*/}
+                </div>
+            </div>
+        )
+    }
+}
